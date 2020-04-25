5c316ae910d23c1bd48535242d49c27d88030f97
Metin Yorgun <30695695+mtnyrgn@users.noreply.github.com>
2020-04-26 00:38:48 +0300

    update main app
    assets image show on main page

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,23 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar:AppBar(
-          title: Text("I am Rich"),
-        )
-      ),
-      
-      ),
+          title: Text("I am!"),
+          backgroundColor: Colors.blueGrey[900],
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.access_alarm), onPressed: () => print("Hello Alarm"))
+          ],
+          // leading: GestureDetector(
+          //   onTap: () => print("Hello Gesture Detector!"),
+          //   child: Icon(Icons.menu),
+          // ),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+            )
+          ),
+      ),  
+    ),
   );
 }
